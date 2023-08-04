0882e36741cff90cd6ac89b6221c4fc0da22118c
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-04 16:01:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ import uuid
 from werkzeug.utils import secure_filename
 from flask_bcrypt import Bcrypt
 import stripe
-from cryptography.fernet import Fernet
 import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad,unpad
 
 app = Flask(__name__)
 app.secret_key = secret_key
@@ -50,47 +51,13 @@ app.config['UPLOAD_FOLDERSS'] = os.path.join(os.path.dirname(os.path.abspath(__f
 
 bcrypt = Bcrypt(app)
 
-
-
-class Encryptor:
-    def key_create(self):
-        key = Fernet.generate_key()
-        return key
-
-    def key_write(self, key, key_name):
-        with open(key_name, 'wb') as mykey:
-            mykey.write(key)
-
-    def key_load(self, key_name):
-        with open(key_name, 'rb') as mykey:
-            key = mykey.read()
-        return key
-
-    def file_encrypt(self, key, original_file, encrypted_file):
-        f = Fernet(key)
-        with open(original_file, 'rb') as file:
-            original = file.read()
-        encrypted = f.encrypt(original)
-        with open(encrypted_file, 'wb') as file:
-            file.write(encrypted)
-
-    def file_decrypt(self, key, encrypted_file, decrypted_file):
-        f = Fernet(key)
-        with open(encrypted_file, 'rb') as file:
-            encrypted = file.read()
-        decrypted = f.decrypt(encrypted)
-        with open(decrypted_file, 'wb') as file:
-            file.write(decrypted)
-
-
-
 class Eazypay:
-    def __init__(self, encryption_key):
+    def __init__(self):
         self.merchant_id = '600541'
-        self.encryption_key = encryption_key
+        self.encryption_key = b'6000012605405020'
         self.sub_merchant_id = '45'
         self.paymode = '9'
-        self.return_url = ' https://doctorsolympiad.com/purchase-summary/order-received/'
+        self.return_url = 'https://doctorsolympiad.com/purchase-summary/order-received/'
 
     def get_payment_url(self, reference_no, amount,name,email, phone,optional_field=None):
         mandatory_field = self.get_mandatory_field(reference_no, amount,name,email,phone)
@@ -108,11 +75,11 @@ class Eazypay:
     def generate_payment_url(self, mandatory_field, optional_field, reference_no, amount):
         
         encrypted_url = (
-            f"https://eazypay.icicibank.com/EazyPG?merchantid={self.merchant_id}"
-            f"&mandatory fields={mandatory_field.decode('utf-8')}&optional fields={optional_field}"
-            f"&returnurl={self.get_return_url().decode('utf-8')}&Reference No={reference_no.decode('utf-8')}"
-            f"&submerchantid={self.get_sub_merchant_id().decode('utf-8')}&transaction amount={amount.decode('utf-8')}"
-            f"&paymode={self.get_paymode().decode('utf-8')}"
+            f"https://eazypayuat.icicibank.com/EazyPG?merchantid={self.merchant_id}"
+            f"&mandatory fields={mandatory_field}&optional fields={optional_field}"
+            f"&returnurl={self.get_return_url()}&Reference No={reference_no}"
+            f"&submerchantid={self.get_sub_merchant_id()}&transaction amount={amount}"
+            f"&paymode={self.get_paymode()}"
         )
         return encrypted_url
 
@@ -123,19 +90,23 @@ class Eazypay:
     def get_optional_field(self, optional_field=None):
         if optional_field is not None:
             return self.get_encrypted_value(optional_field)
-        return None
+        return ''
 
 
     def get_encrypted_value(self, data):
-            f = Fernet(self.encryption_key)
-            data = data.encode('utf-8')
-            encrypted_data = f.encrypt(data)
-            return encrypted_data
+        cipher = AES.new(self.encryption_key, AES.MODE_ECB)
+        padded_plaintext = pad(data.encode('utf-8'), AES.block_size)
+        ciphertext = cipher.encrypt(padded_plaintext)
+        encrypted_base64 = base64.b64encode(ciphertext)
+        return encrypted_base64.decode('utf-8')
 
-    def decrypt_data(self, encrypted_data):
-        f = Fernet(self.encryption_key)
-        decrypted_data = f.decrypt(encrypted_data)
-        return decrypted_data.decode('utf-8')
+    def decrypt(self, encrypted_data):
+        cipher = AES.new(self.encryption_key, AES.MODE_ECB)
+        encrypted_data_bytes = base64.urlsafe_b64decode(encrypted_data.encode('utf-8'))
+        decrypted_data = cipher.decrypt(encrypted_data_bytes)
+        unpadded_data = unpad(decrypted_data, AES.block_size)
+        return unpadded_data.decode('utf-8')
+
 
     def get_return_url(self):
         return self.get_encrypted_value(self.return_url)
@@ -146,25 +117,161 @@ class Eazypay:
     def get_paymode(self):
         return self.get_encrypted_value(self.paymode)
 
-def generate_encryption_key():
-    encryptor = Encryptor()
-    # Convert the 16-byte key to a 32-byte key and base64-encode it
-    key_128_bit = b'6000012605405020'
-    encryption_key_256_bit = base64.urlsafe_b64encode(key_128_bit.ljust(32, b'\x00'))
+def payment_success_exec(response):
+    if 'Total_Amount' in response and response['Response_Code'] == 'E000':
+        aes_key_for_payment_success = '6000012605405020'
+        data = {
+            'Response_Code': response['Response_Code'],
+            'Unique_Ref_Number': response['Unique_Ref_Number'],
+            'Service_Tax_Amount': response['Service_Tax_Amount'],
+            'Processing_Fee_Amount': response['Processing_Fee_Amount'],
+            'Total_Amount': response['Total_Amount'],
+            'Transaction_Amount': response['Transaction_Amount'],
+            'Transaction_Date': response['Transaction_Date'],
+            'Interchange_Value': response['Interchange_Value'],
+            'TDR': response['TDR'],
+            'Payment_Mode': response['Payment_Mode'],
+            'SubMerchantId': response['SubMerchantId'],
+            'ReferenceNo': response['ReferenceNo'],
+            'ID': response['ID'],
+            'RS': response['RS'],
+            'TPS': response['TPS'],
+        }
 
-    # Save the encryption key to the file 'mykey.key'
-    encryptor.key_write(encryption_key_256_bit, 'mykey.key')
+        # Calculate the verification key
+        verification_key = (
+            data['ID'] + '|' + data['Response_Code'] + '|' + data['Unique_Ref_Number'] + '|' +
+            data['Service_Tax_Amount'] + '|' + data['Processing_Fee_Amount'] + '|' + data['Total_Amount'] + '|' +
+            data['Transaction_Amount'] + '|' + data['Transaction_Date'] + '|' + data['Interchange_Value'] + '|' +
+            data['TDR'] + '|' + data['Payment_Mode'] + '|' + data['SubMerchantId'] + '|' + data['ReferenceNo'] + '|' +
+            data['TPS'] + '|' + aes_key_for_payment_success
+        )
 
-    return encryption_key_256_bit
+        # Encrypt the verification key using sha512
+        eazypay_integration = Eazypay()
+        encrypted_message = eazypay_integration.get_encrypted_value(verification_key)
 
+        # encrypted_message = Eazypay.get_encrypted_value(verification_key)
 
+        if encrypted_message == data['RS']:
+            return True
 
+    return False
 
-def load_encryption_key():
-    # Load the encryption key from the file 'mykey.key'
-    encryptor = Encryptor()
-    encryption_key = encryptor.key_load('mykey.key')
-    return encryption_key
+def response_code(code):
+    rc = {
+        'E000': 'Payment Successful.',
+        'E001': 'Unauthorized Payment Mode',
+        'E002': 'Unauthorized Key',
+        'E003' :'Unauthorized Packet', 
+        'E004' :'Unauthorized Merchant', 
+        'E005' :'Unauthorized Return URL', 
+        'E006' :'"Transaction Already Paid, Received Confirmation from the Bank, Yet to Settle the transaction with the Bank', 
+        'E007' :'Transaction Failed', 
+        'E008' :'Failure from Third Party due to Technical Error', 
+        'E009' :'Bill Already Expired', 
+        'E0031' :'Mandatory fields coming from merchant are empty', 
+        'E0032' :'Mandatory fields coming from database are empty', 
+        'E0033' :'Payment mode coming from merchant is empty', 
+        'E0034' :'PG Reference number coming from merchant is empty', 
+        'E0035' :'Sub merchant id coming from merchant is empty', 
+        'E0036' :'Transaction amount coming from merchant is empty', 
+        'E0037' :'Payment mode coming from merchant is other than 0 to 9', 
+        'E0038' :'Transaction amount coming from merchant is more than 9 digit length', 
+        'E0039' :'Mandatory value Email in wrong format', 
+        'E00310' :'Mandatory value mobile number in wrong format', 
+        'E00311' :'Mandatory value amount in wrong format', 
+        'E00312' :'Mandatory value Pan card in wrong format', 
+        'E00313' :'Mandatory value Date in wrong format', 
+        'E00314' :'Mandatory value String in wrong format', 
+        'E00315' :'Optional value Email in wrong format', 
+        'E00316' :'Optional value mobile number in wrong format', 
+        'E00317' :'Optional value amount in wrong format', 
+        'E00318' :'Optional value pan card number in wrong format', 
+        'E00319' :'Optional value date in wrong format', 
+        'E00320' :'Optional value string in wrong format', 
+        'E00321' :'Request packet mandatory columns is not equal to mandatory columns set in enrolment or optional columns are not equal to optional columns length set in enrolment', 
+        'E00322' :'Reference Number Blank', 
+        'E00323' :'Mandatory Columns are Blank', 
+        'E00324' :'Merchant Reference Number and Mandatory Columns are Blank', 
+        'E00325' :'Merchant Reference Number Duplicate', 
+        'E00326' :'Sub merchant id coming from merchant is non numeric', 
+        'E00327' :'Cash Challan Generated', 
+        'E00328' :'Cheque Challan Generated', 
+        'E00329' :'NEFT Challan Generated', 
+        'E00330' :'Transaction Amount and Mandatory Transaction Amount mismatch in Request URL', 
+        'E00331' :'UPI Transaction Initiated Please Accept or Reject the Transaction', 
+        'E00332' :'Challan Already Generated, Please re-initiate with unique reference number', 
+        'E00333' :'Referer value is null / invalid Referer', 
+        'E00334' :'Value of Mandatory parameter Reference No and Request Reference No are not matched', 
+        'E00335' :'Payment has been cancelled',
+        'E0801' :'FAIL', 
+        'E0802' :'User Dropped', 
+        'E0803' :'Canceled by user', 
+        'E0804' :'User Request arrived but card brand not supported', 
+        'E0805' :'Checkout page rendered Card function not supported', 
+        'E0806' :'Forwarded / Exceeds withdrawal amount limit', 
+        'E0807' :'PG Fwd Fail / Issuer Authentication Server failure', 
+        'E0808' :'Session expiry / Failed Initiate Check, Card BIN not present', 
+        'E0809' :'Reversed / Expired Card', 
+        'E0810' :'Unable to Authorize', 
+        'E0811' :'Invalid Response Code or Guide received from Issuer', 
+        'E0812' :'Do not honor', 
+        'E0813' :'Invalid transaction', 
+        'E0814' :'Not Matched with the entered amount', 
+        'E0815' :'Not sufficient funds', 
+        'E0816' :'No Match with the card number', 
+        'E0817' :'General Error', 
+        'E0818' :'Suspected fraud', 
+        'E0819' :'User Inactive', 
+        'E0820' :'ECI 1 and ECI6 Error for Debit Cards and Credit Cards', 
+        'E0821' :'ECI 7 for Debit Cards and Credit Cards', 
+        'E0822' :'System error. Could not process transaction', 
+        'E0823' :'Invalid 3D Secure values', 
+        'E0824' :'Bad Track Data', 
+        'E0825' :'Transaction not permitted to cardholder', 
+        'E0826' :'Rupay timeout from issuing bank', 
+        'E0827' :'OCEAN for Debit Cards and Credit Cards', 
+        'E0828' :'E-commerce decline', 
+        'E0829' :'This transaction is already in process or already processed', 
+        'E0830' :'Issuer or switch is inoperative', 
+        'E0831' :'Exceeds withdrawal frequency limit', 
+        'E0832' :'Restricted card', 
+        'E0833' :'Lost card', 
+        'E0834' :'Communication Error with NPCI', 
+        'E0835' :'The order already exists in the database', 
+        'E0836' :'General Error Rejected by NPCI', 
+        'E0837' :'Invalid credit card number', 
+        'E0838' :'Invalid amount', 
+        'E0839' :'Duplicate Data Posted', 
+        'E0840' :'Format error', 
+        'E0841' :'SYSTEM ERROR', 
+        'E0842' :'Invalid expiration date', 
+        'E0843' :'Session expired for this transaction', 
+        'E0844' :'FRAUD - Purchase limit exceeded', 
+        'E0845' :'Verification decline', 
+        'E0846' :'Compliance error code for issuer', 
+        'E0847' :'Caught ERROR of type:[ System.Xml.XmlException ] . strXML is not a valid XML string', 
+        'E0848' :'Incorrect personal identification number', 
+        'E0849' :'Stolen card', 
+        'E0850' :'Transaction timed out, please retry', 
+        'E0851' :'Failed in Authorize - PE', 
+        'E0852' :'Cardholder did not return from Rupay', 
+        'E0853' :'Missing Mandatory Field(s)The field card_number has exceeded the maximum length of', 
+        'E0854' :'Exception in CheckEnrollmentStatus: Data at the root level is invalid. Line 1, position 1.', 
+        'E0855' :'CAF status = 0 or 9', 
+        'E0856' :'412', 
+        'E0857' :'Allowable number of PIN tries exceeded', 
+        'E0858' :'No such issuer', 
+        'E0859' :'Invalid Data Posted', 
+        'E0860' :'PREVIOUSLY AUTHORIZED', 
+        'E0861' :'Cardholder did not return from ACS', 
+        'E0862' :'Duplicate transmission', 
+        'E0863' :'Wrong transaction state', 
+        'E0864' :'Card acceptor contact acquirer',
+    }
+
+    return rc.get(code, 'Unknown Error')
 
 @app.route('/')
 def home():
@@ -947,7 +1054,60 @@ def registeredgame(game):
         return render_template(f'/games-individual-team/Individual/{game}.html',gender=gender)
     else:
         return render_template(f'/games-individual-team/Team/{game}.html',gender=gender)
+
+
+@app.route('/pays')
+def pays():
+    return render_template('eazy.html')
+
+@app.route('/checkout/order-pay')
+def paymentsss():
+    return render_template('payment_form.html')
+
+def validate_form(amount):
+    if amount < 0:
+        return False
+    return True
+
+
+@app.route('/checkout/order-pay', methods=['POST'])
+def process_payment():
+    session['payer_name'] = request.form['payername']
+    session['payer_phone'] = request.form['payerphone']
+    session['payer_email'] = request.form['payeremail']
+    amount = int(request.form['payeramount'])
+    name = session['payer_name']
+    email = session['payer_email']
+    phone = session['payer_phone']
+
+    if not validate_form(amount):
+        return "Amount should be greater than Rs.10"
+
+    # Create an instance of Eazypay with the encryption key
+    eazypay_integration = Eazypay()
+
+    # reference_no = 8001  # You can use a random number generator if you prefer
+    reference_no = str(random.randint(100000, 999999))
+
+    payment_url = eazypay_integration.get_payment_url(reference_no, amount,name,email, phone)
+    print(payment_url)
     
+    return redirect(payment_url)
+
+
+
+@app.route('/purchase-summary/order-received/', methods=['POST'])
+def response_handler():
+    response = request.form.to_dict()
+
+    if payment_success_exec(response):
+        print(response)
+        # Payment is successful
+        return render_template('thank-you.html')
+    else:
+        # Payment failed, show failure message
+        response_msg = response_code(response['Response_Code'])
+        return f"Transaction failed. Error: {response_msg}"
 
 
 if __name__ == '__main__':
