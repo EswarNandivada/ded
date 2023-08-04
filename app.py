c6eeac66cb9028a0a62e57745289710886850b7e
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-04 17:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,12 +146,12 @@ def payment_success_exec(response):
             data['TDR'] + '|' + data['Payment_Mode'] + '|' + data['SubMerchantId'] + '|' + data['ReferenceNo'] + '|' +
             data['TPS'] + '|' + aes_key_for_payment_success.decode('utf-8')  # Decode bytes to string
         )
-
-         print(verification_key)
+          
 
         # Encrypt the verification key using sha512
         eazypay_integration = Eazypay()
         encrypted_message = eazypay_integration.get_encrypted_value(verification_key)
+        print(verification_key)
 
         # encrypted_message = Eazypay.get_encrypted_value(verification_key)
 
