4975c90f95adbd85ff8cdc18d356123dd346e77f
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-06 20:57:45 +0530

    Update cmail.py

--- cmail.py
+++ cmail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('datta@codegnan.com','nsvrdwpuefhlqawu')
+    server.login('docolympiad2023@gmail.com','doqxthdplyncmpxv')
     msg=EmailMessage()
     msg['From']='datta@codegnan.com'
     msg['Subject']=subject
@@ -10,3 +10,19 @@ def sendmail(to,subject,body):
     msg.set_content(body)
     server.send_message(msg)
     server.quit()
+def mail_with_atc(to,subject,body,html):
+    email_message = MIMEMultipart()
+    email_message['From'] = email_from
+    email_message['To'] = email_to
+    email_message['Subject'] = f'Report email - {date_str}'
+
+    # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
+    email_message.attach(MIMEText(html, "html"))
+    # Convert it as a string
+    email_string = email_message.as_string()
+
+    # Connect to the Gmail SMTP server and Send Email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login('docolympiad2023@gmail.com', 'doqxthdplyncmpxv')
+        server.sendmail(email_from, email_to, email_string)
