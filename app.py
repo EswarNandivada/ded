01d9540fcdd13f2ef48a119b2bc9c11a951aac5a
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-06 22:24:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,9 @@ bcrypt = Bcrypt(app)
 
 class Eazypay:
     def __init__(self):
-        self.merchant_id = '600541'
-        self.encryption_key = b'6000012605405020'
-        self.sub_merchant_id = '45'
+        self.merchant_id = '376890'
+        self.encryption_key = b'37770031689010000'
+        self.sub_merchant_id = '20'
         self.paymode = '9'
         self.return_url = f'https://doctorsolympiad.com/purchase-summary/order-received/'
 
@@ -93,14 +93,14 @@ class Eazypay:
         merchant_ids = self.decrypt(self.get_sub_merchant_id())
         pay_modes = self.decrypt(self.get_paymode())
         encrypted_url = (
-            f"https://eazypayuat.icicibank.com/EazyPG?merchantid={self.merchant_id}"
+            f"https://eazypay.icicibank.com/EazyPG?merchantid={self.merchant_id}"
             f"&mandatory fields={mandatory_field}&optional fields={optional_field}"
             f"&returnurl={self.get_return_url()}&Reference No={reference_no}"
             f"&submerchantid={self.get_sub_merchant_id()}&transaction amount={amount}"
             f"&paymode={self.get_paymode()}"
         )
         decrypted_url = (
-            f"https://eazypayuat.icicibank.com/EazyPG?merchantid={self.merchant_id}"
+            f"https://eazypay.icicibank.com/EazyPG?merchantid={self.merchant_id}"
             f"&mandatory fields={mandatory_fields}&optional fields={optional_field}"
             f"&returnurl={return_urls}&Reference No={reference_nos}"
             f"&submerchantid={merchant_ids}&transaction amount={amounts}"
@@ -153,7 +153,7 @@ def payment_success_exec():
         res = request.form
         
         # Same encryption key that we gave for generating the URL
-        aes_key_for_payment_success = '6000012605405020'  # Replace this with the actual key
+        aes_key_for_payment_success = '3777003168901000'  # Replace this with the actual key
 
         data = {
             'Response_Code': res['Response Code'],
@@ -749,7 +749,7 @@ def success():
             #     </table>
             # </body>
             # </html>'''%(uid,name,email,game,transaction_id,amount)
-            # session['user']=uid
+            session['user']=uid
             # subject = 'Payment Successful! From Doctors Olympiad 2023'
             # mail_with_atc(email,subject,html)
             subject='IMA Doctors Olympiad Registration'
