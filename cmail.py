6a0ceb5933c6f69027cd1fc9b944e020b86b11d5
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-07 17:35:14 +0530

    Update cmail.py

--- cmail.py
+++ cmail.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('docolympiad2023@gmail.com','doqxthdplyncmpxv')
+    server.login('datta@codegnan.com','nsvrdwpuefhlqawu')
     msg=EmailMessage()
     msg['From']='datta@codegnan.com'
     msg['Subject']=subject
@@ -14,7 +14,7 @@ def sendmail(to,subject,body):
     server.quit()
 def mail_with_atc(to,subject,html):
     email_message = MIMEMultipart()
-    email_message['From'] = 'docolympiad2023@gmail.com'
+    email_message['From'] = 'datta@codegnan.com'
     email_message['To'] = to
     email_message['Subject'] = subject
 
@@ -26,5 +26,5 @@ def mail_with_atc(to,subject,html):
     # Connect to the Gmail SMTP server and Send Email
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login('docolympiad2023@gmail.com', 'doqxthdplyncmpxv')
+        server.login('datta@codegnan.com','nsvrdwpuefhlqawu')
         server.sendmail(email_from, email_to, email_string)
