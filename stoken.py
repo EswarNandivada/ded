9a41b3bdb634cc6107e900f20ef754920a4d579d
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-07 21:06:36 +0530

    Update stoken.py

--- stoken.py
+++ stoken.py
@@ -3,3 +3,9 @@ from key import secret_key
 def token(email,salt):
     serializer= URLSafeTimedSerializer(secret_key)
     return serializer.dumps(email,salt=salt)
+def token2(teamid,rid,salt,email=None):
+    if email==None:
+        serializer= URLSafeTimedSerializer(secret_key)
+        return serializer.dumps({rid:teamid},salt=salt)
+    else:
+        return serializer.dumps({rid:[teamid,email]},salt=salt)
