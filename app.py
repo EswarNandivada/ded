851869e974ebe00b83172b7b6c34e294872509af
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-08 11:42:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class Eazypay:
         self.encryption_key = b'6000012605405020'
         self.sub_merchant_id = '45'
         self.paymode = '9'
-        self.return_url = 'https://doctorsolympiad.com/purchase-summary/order-received/'
+        self.return_url = 'https://doctorsolympiad.com/success'
 
     def get_payment_url(self, reference_no, amount,name,email, phone,optional_field=None):
         mandatory_field = self.get_mandatory_field(reference_no, amount,name,email,phone)
@@ -682,7 +682,7 @@ def payment(eid,game,amount):
 #     return redirect(checkout_session.url)
 
 
-@app.route('/purchase-summary/order-received/',methods=['POST'])
+@app.route('/success',methods=['POST'])
 def success():
     response = request.form.to_dict()
     print(response)
