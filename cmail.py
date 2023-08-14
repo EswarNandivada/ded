b5179aedd9017fd473188783c6eaa77e0b447652
EswarNandivada <64798695+EswarNandivada@users.noreply.github.com>
2023-08-14 16:15:08 +0530

    Update cmail.py

--- cmail.py
+++ cmail.py
@@ -3,10 +3,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.message import EmailMessage
 def sendmail(to,subject,body):
-    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('datta@codegnan.com','nsvrdwpuefhlqawu')
+    server=smtplib.SMTP_SSL('smtp.gmail.com',587)
+    server.login('email-smtp.ap-south-1.amazonaws.com','AKIAZZP24JWSI3KRELFIBKP2K5zFwDvwUH06qtyZl6dox5o0AUsMt+KIkzSzRl55')
     msg=EmailMessage()
-    msg['From']='datta@codegnan.com'
+    msg['From']='email-smtp.ap-south-1.amazonaws.com'
     msg['Subject']=subject
     msg['To']=to
     msg.set_content(body)
@@ -14,7 +14,7 @@ def sendmail(to,subject,body):
     server.quit()
 def mail_with_atc(to,subject,html):
     email_message = MIMEMultipart()
-    email_message['From'] = 'datta@codegnan.com'
+    email_message['From'] = 'email-smtp.ap-south-1.amazonaws.com'
     email_message['To'] = to
     email_message['Subject'] = subject
 
@@ -25,6 +25,6 @@ def mail_with_atc(to,subject,html):
 
     # Connect to the Gmail SMTP server and Send Email
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login('datta@codegnan.com','nsvrdwpuefhlqawu')
-        server.sendmail('datta@codegnan.com', to, email_string)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 587, context=context) as server:
+        server.login('email-smtp.ap-south-1.amazonaws.com','AKIAZZP24JWSI3KRELFIBKP2K5zFwDvwUH06qtyZl6dox5o0AUsMt+KIkzSzRl55')
+        server.sendmail('email-smtp.ap-south-1.amazonaws.com', to, email_string)
